0fe407e898e32f65ac9a9b7a631b12abee6dcef6
Steven Elberger <stevenelberger@gmail.com>
2015-02-13 23:51:35 -0800

    Security and required fields are done for createaccount.php

--- createaccount.php
+++ createaccount.php
@@ -3,17 +3,25 @@
 <html>
 	<head>
 		<title>HealthMate Login</title>
+		<style>
+			.required {
+				color: #FF0000;
+			}
+		</style>
 	</head>
 	<body>
 		<?php
+			// Flag for first load
+			$firstLoad = true;
 			// Error placeholders
 			$firstNameError = $lastNameError = $usernameError = $mismatchError = "";
-			$passwordError = $confirmError = $companyError = $phoneError = "";
+			$passwordError = $confirmError = $companyError = $phoneError = $requiredFields = "";
 			// Placeholders for variables from form
 			$username = $password = $confirm = $first_name = $last_name = $company = $phone = "";
 			
 			// Only process POST requests, not GET
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				$firstLoad = false;
 				// Check the required fields
 				if (empty($_POST["first_name"])) {
 					$firstNameError = "First name is required";
@@ -36,7 +44,7 @@
 				if (empty($_POST["password"])) {
 					$passwordError = "Password is required";
 				} else {
-					$password = test_input($_POST["username"]);
+					$password = test_input($_POST["password"]);
 				}
 				
 				if (empty($_POST["confirm"])) {
@@ -51,30 +59,30 @@
 					$company = test_input($_POST["company"]);
 				}
 				
-				if (empty($_POST["phone"]) {
+				if (empty($_POST["phone"])) {
 					$phoneError = "Phone number is required";
 				} else {
 					$phone = test_input($_POST["phone"]);
 				}
 				
-				if ($password != $confirm) {
+				if ($password !== $confirm) {
 					$mismatchError = "Passwords do not match";
 				}
 			}
 			
 			// As long as all variables were initialized, the data is good to go
-			if (!empty($first_name) && !empty($last_name) && !empty($username) && !empty(company)
-			&& !empty($phone)) {
+			if (($first_name !== "") && ($last_name !== "") && ($username !== "") && ($company !== "")
+			&& ($phone !== "") && ($password !== "") && ($confirm !== "") && ($mismatchError === "")) {
 			
 				// Some DB info - users use the HMTest user
 				// and tests are done on the testdb database
 				$servername = "localhost";
-				$username = "HMTest";
-				$password = "comp490";
+				$dbusername = "HMTest";
+				$dbpassword = "comp490";
 				$dbname = "testdb";
 
 				// Create connection
-				$conn = new mysqli($servername, $username, $password, $dbname);
+				$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 				// Check connection
 				if ($conn->connect_error) {
@@ -97,6 +105,10 @@
 			
 				// Peace out
 				$conn->close();
+			} else {
+				if (!$firstLoad) {
+					$requiredFields = "The following fields are required: ";
+				}
 			}
 			
 			// Removes unwanted and potentially malicious characters
@@ -110,14 +122,22 @@
 		?>
 		
 		<h1>HealthMate Physician Login</h1>
+		<p><span class="required"><?php echo $requiredFields; ?></span></p>
 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-		<p>First name: <input type="text" name="first_name" size="16" maxlength="16" /></p>
-		<p>Last name: <input type="text" name="last_name" size="16" maxlength="16" /></p>
-		<p>Username: <input type="text" name="username" size="16" maxlength="16" /></p>
-		<p>Password: <input type="password" name="password" size="16" maxlength="16" /></p>
-		<p>Confirm Password: <input type="password" name="confirm" size="16" maxlength="16" /></p>
-		<p>Company: <input type="text" name="company" size="16" maxlength="16" /></p>
-		<p>Phone Number: <input type="text" name="phone" size="16" maxlength="16" /></p>
+		<p>First name: <input type="text" name="first_name" size="16" maxlength="16" />
+		<span class="required"><?php echo $firstNameError; ?></span></p>
+		<p>Last name: <input type="text" name="last_name" size="16" maxlength="16" />
+		<span class="required"><?php echo $lastNameError; ?></span></p>
+		<p>Username: <input type="text" name="username" size="16" maxlength="16" />
+		<span class="required"><?php echo $usernameError; ?></span></p>
+		<p>Password: <input type="password" name="password" size="16" maxlength="16" />
+		<span class="required"><?php echo $passwordError; ?></span></p>
+		<p>Confirm Password: <input type="password" name="confirm" size="16" maxlength="16" />
+		<span class="required"><?php echo $confirmError; ?><?php echo $mismatchError; ?></span></p>
+		<p>Company: <input type="text" name="company" size="16" maxlength="16" />
+		<span class="required"><?php echo $companyError; ?></span></p>
+		<p>Phone Number: <input type="text" name="phone" size="16" maxlength="16" />
+		<span class="required"><?php echo $phoneError; ?></span></p>
 		<input type="submit" value="Submit" />
 	</body>
 </html>
