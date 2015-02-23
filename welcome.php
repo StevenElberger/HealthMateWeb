ec460d45143498394c8ee46586e5cdde96f729f0
Steven Elberger <stevenelberger@gmail.com>
2015-02-23 00:04:27 -0800

    View rudimentary patient list complete

--- welcome.php
+++ welcome.php
@@ -8,7 +8,10 @@
 			session_start();
 			$username = $_SESSION["username"];
 			
-			function rand_patient() {
+			$patient_info = "";
+			
+			// Only process POST requests, not GET
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				// Some DB info - users use the HMTest user
 				// and tests are done on the testdb database
 				$servername = "localhost";
@@ -24,13 +27,24 @@
 					die("Connection failed: " . $conn->connect_error);
 				}
 				
+				// For now, all patients will be shown (table needs to be re-designed
 				$sql = "SELECT * FROM patient WHERE group_id = 2";
 				$result = $conn->query($sql);
 				
+				// Generate the proper HTML elements and display
+				// all the patient info
 				if ($result->num_rows > 0) {
 					// output each row
 					while ($row = $result->fetch_assoc()) {
-						echo "first name: " . $row["first_name"] . "<br />";
+						$patient_info .= "<section class='container'>";
+						$patient_info .= "<div class='login'>";
+						$patient_info .= "Patient ID: " . $row["patient_id"] . "<br />";
+						$patient_info .= "First name: " . $row["first_name"] . "<br />";
+						$patient_info .= "Last name: " . $row["last_name"] . "<br />";
+						$patient_info .= "Gender: " . $row["gender"] . "<br />";
+						$patient_info .= "Birthday: " . $row["birthday"] . "<br />";
+						$patient_info .= "</div>";
+						$patient_info .= "</section>";
 					}
 				} else {
 					echo "Error: " . $sql . "<br />" . $conn->error;
@@ -45,10 +59,13 @@
 		<section class="container">
 			<div class="login">
 				<h1>Welcome</h1>
+				<center>
 				<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-					<input type="submit" value="Patient List" />
+					<input type="submit" value="View Patient List" />
 				</form>
+				</center>
 			</div>
 		</section>
+		<?php echo $patient_info ?>
 	</body>
 </html>
