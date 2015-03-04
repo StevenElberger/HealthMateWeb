4041bf5d6d993dd78074c6505b1b8dbf4ce73dd3
Michael Sandoval <sandovalmichael48@yahoo.com>
2015-03-03 19:27:49 -0800

    Added untracked php files and folders

---
+++ private/reset_token_functions.php
@@ -0,0 +1,113 @@
+<?php
+// Reset token functions
+
+// Function that generates a string that can be used as a reset token. 
+// The function generates this unique token by generating a random number,
+// generating a unique id from that number and then hashing it.
+function reset_token() {
+	return md5(uniqid(rand()));
+}
+
+// Looks up a user and sets their reset_token to
+// the given value. Can be used both to create and
+// to delete the token.
+function set_user_reset_token($username, $token_value) {
+	
+	// Attempt to connect to the database
+   $db = mysqli_connect("localhost", "root", "#mws1992", "testDB");
+   if (mysqli_connect_errno()) {
+      die("Database connection failed: " . mysqli_connect_error() .
+         " (" . mysqli_connect_errno() . ")");
+   }
+   
+   // SQL statement to retrieve rows that have the username column equal to the given username      
+    $sql_statement = "SELECT * FROM users WHERE username='".$username."'";
+
+   // execute query
+   $users = $db->query($sql_statement);
+
+   // check if anything was returned by database
+   if ($users->num_rows > 0) {
+
+      // fetch the first row of the results of the query
+      $row = $users->fetch_assoc();
+
+      // Set reset token for the found user
+      $sql_statement = "UPDATE users SET reset_token='".$token_value."' WHERE username='".$username."'";
+
+      // execute query
+      $db->query($sql_statement);
+      $db->close();
+      return true;
+	} else {
+		return false;
+	}
+}
+
+// Function that generates a new reset token,
+// and sets the reset token for the given user.
+function create_reset_token($username) {
+	$token = reset_token();
+	return set_user_reset_token($username, $token);
+}
+
+// Function that removes the token for the given
+// user by setting the value to null.
+function delete_reset_token($username) {
+	$token = null;
+	return set_user_reset_token($username, $token);
+}
+
+// Returns the user record for a given reset token.
+// If token is not found, returns null.
+function find_user_with_token($token) {
+	if(empty($token)) {
+		// We were expecting a token and didn't get one.
+		return null;
+	} else {
+		
+	   // Attempt to connect to the database
+      $db = mysqli_connect("localhost", "root", "#mws1992", "testDB");
+      if (mysqli_connect_errno()) {
+         die("Database connection failed: " . mysqli_connect_error() .
+           " (" . mysqli_connect_errno() . ")");
+      }
+   
+      // SQL statement to retrieve rows that have the username column equal to the given username      
+      $sql_statement = "SELECT * FROM users WHERE reset_token='".$token."'";
+
+      // execute query
+      $users = $db->query($sql_statement);
+      
+      // check if anything was returned by database
+      if ($users->num_rows > 0) {
+
+         // fetch the first row of the results of the query
+         $row = $users->fetch_assoc();
+		
+		   // return the username associated with the reset_token if found
+		   // otherwise return null
+		   return $row["username"];
+	   } else {
+		   return null;
+	   }
+	}
+}
+
+// A function to email the reset token to the email
+// address on file for this user.
+// This is a placeholder since we don't have email
+// abilities set up in the demo version.
+function email_reset_token($username) {
+	$user = "";//= find_one_in_fake_db('users', 'username', sql_prep($username));
+	
+	if($user) {
+		// This is where you would connect to your emailer
+		// and send an email with a URL that includes the token.
+		return true;
+	} else {
+		return false;
+	}
+}
+
+?>
---
+++ public/forgot_password.php
@@ -0,0 +1,88 @@
+<?php require_once("../private/initialize.php"); ?>
+
+<?php
+session_start();
+
+// initialize variables to default values
+$username = "";
+$message = "";
+
+if(request_is_post() && request_is_same_domain()) {
+	
+  if(!csrf_token_is_valid() || !csrf_token_is_recent()) {
+  	$message = "Sorry, request was not valid.";
+  } else {
+    // CSRF tests passed--form was created by us recently.
+
+		// retrieve the values submitted via the form
+    $username = $_POST['username'];
+    
+		if(!empty($username)) {
+			
+			// Search our fake database to retrieve the user data
+			// Attempt to connect to the database
+         $db = mysqli_connect("localhost", "root", "#mws1992", "testDB");
+         if (mysqli_connect_errno()) {
+            die("Database connection failed: " . mysqli_connect_error() .
+              " (" . mysqli_connect_errno() . ")");
+         }
+   
+         // SQL statement to retrieve rows that have the username column equal to the given username      
+         $sql_statement = "SELECT * FROM users WHERE username='".$username."'";
+         
+         // execute query
+         $users = $db->query($sql_statement);
+      
+         // check if anything was returned by database
+         if ($users->num_rows > 0) {
+            // fetch the first row of the results of the query
+            $row = $users->fetch_assoc();
+            $user = $row['username'];
+
+	         if($user) {
+				   // Username was found; okay to reset
+				   echo 'reset';
+				   create_reset_token($username);
+				   email_reset_token($username);
+	          } else {
+	            // Username was not found; don't do anything
+	          }
+			 }
+	
+			// Message returned is the same whether the user 
+			// was found or not, so that we don't reveal which 
+			// usernames exist and which do not.
+			$message = "A link to reset your password has been sent to the email address on file.";
+		
+		} else {
+			$message = "Please enter a username.";
+		}
+  }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Forgot Password</title>
+  </head>
+  <body>
+    
+    <?php
+      if($message != "") {
+        echo '<p>' . sanitize_html($message) . '</p>';
+      }
+    ?>
+    
+    <p>Enter your username to reset your password.</p>
+    
+    <form action="forgot_password.php" method="POST" accept-charset="utf-8">
+      <?php echo csrf_token_tag(); ?>
+      Username: <input type="text" name="username" value="<?php echo sanitize_html($username); ?>" /><br />
+			<br />
+      <input type="submit" name="submit" value="Submit" />
+    </form>
+  </body>
+</html>
