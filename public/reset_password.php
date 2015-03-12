8cf22c4c982c6e7ffc44929c964f78ba6715f7cc
Michael Sandoval <sandovalmichael48@yahoo.com>
2015-03-12 11:14:40 -0700

    Added Nav bar on the top of forget password and reset password

--- public/reset_password.php
+++ public/reset_password.php
@@ -79,8 +79,38 @@ if(request_is_post() && request_is_same_domain()) {
     <!-- Custom CSS for Login -->
     <link href="../newcss/login.css" type="text/css" rel="stylesheet">
   </head>
-  <body style="padding-top: 0px">
-    <h1 class="btn-primary" style="padding-top: 0px">HealthMate</h1>
+  <body>
+    <!-- begin navigation bar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="">HealthMate</a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="">Home<span class="sr-only">(current)</span></a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Patient <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Add Patient</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">View Patient List</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="">Settings</a></li>
+                        <li><a href="">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- end navigation bar -->
 
     <?php
       if($message != "") {
